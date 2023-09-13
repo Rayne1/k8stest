2b1a3423525c2700f076c9eba09c816394abc25b
Artsiom <i.rayne.a3@gmail.com>
2023-09-13 22:27:10 +0200

    add test code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using k8s;
+
 Console.WriteLine("Hello, World!");
+await Task.Delay (10);
+System.Console.WriteLine("Do request");
+
+var config = KubernetesClientConfiguration.InClusterConfig();
+var client = new Kubernetes(config);
+
+var services = await client.ListNamespacedServiceAsync("warranty");
